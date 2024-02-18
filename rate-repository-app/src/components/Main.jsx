a34ee04cc77f38aab55f63adee38649a7d3f35ea
xakyth <xakyth@gmail.com>
2024-02-18 19:13:36 +0300

    added routes

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -2,12 +2,13 @@ import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 import theme from '../theme';
+import { Navigate, Route, Routes } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
-    backgroundColor: theme.colors.background
+    backgroundColor: theme.colors.background,
   },
 });
 
@@ -15,7 +16,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate replace to='/' />} />
+      </Routes>
     </View>
   );
 };
