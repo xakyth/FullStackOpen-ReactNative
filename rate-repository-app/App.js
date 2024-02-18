9b045b62a2e6e4f80b3f088b42d7a7bbe8404301
xakyth <xakyth@gmail.com>
2024-02-18 19:11:26 +0300

    react-router-native added

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,7 +1,14 @@
+import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
-  return <Main />;
+  return (<>
+    <NativeRouter>
+      <Main />
+      <StatusBar style='auto' />
+    </NativeRouter>
+  </>);
 };
 
 export default App;
