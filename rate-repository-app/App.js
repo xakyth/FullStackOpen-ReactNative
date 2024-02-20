3e4eab4f67f0487b20e7b7c4403674565c37e149
xakyth <xakyth@gmail.com>
2024-02-20 07:15:55 +0300

    setup apollo client

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,14 +1,22 @@
 import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar';
+import createApolloClient from './src/utils/apolloClient';
+import { ApolloProvider } from '@apollo/client';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
-  return (<>
-    <NativeRouter>
-      <Main />
-      <StatusBar style='auto' />
-    </NativeRouter>
-  </>);
+  return (
+    <>
+      <NativeRouter>
+        <ApolloProvider client={apolloClient}>
+          <Main />
+          <StatusBar style='auto' />
+        </ApolloProvider>
+      </NativeRouter>
+    </>
+  );
 };
 
 export default App;
