61062b5afddd1ed06435e551e3d3835f2a0ac59a
xakyth <xakyth@gmail.com>
2024-02-22 06:32:02 +0300

    passing token from apollo client

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,8 +3,10 @@ import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar';
 import createApolloClient from './src/utils/apolloClient';
 import { ApolloProvider } from '@apollo/client';
+import AuthStorage from './src/utils/authStorage';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
