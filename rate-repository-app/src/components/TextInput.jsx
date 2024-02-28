f39628365efc82fa659fc52995021cb9accc51e1
xakyth <xakyth@gmail.com>
2024-02-28 09:50:45 +0300

    test

--- rate-repository-app/src/components/TextInput.jsx
+++ rate-repository-app/src/components/TextInput.jsx
@@ -2,7 +2,7 @@ import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({});
 
-const TextInput = ({ style, error, ...props }) => {
+const TextInput = ({ style, _error, ...props }) => {
   const textInputStyle = [style];
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
